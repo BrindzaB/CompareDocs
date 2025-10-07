7cb12816288db13bdaa9393385e487bb22a44b09
BrindzaB <63659791+BrindzaB@users.noreply.github.com>
2025-10-07 17:17:02 +0200

    general responsiveness fixes

--- frontend/src/components/FileInput.jsx
+++ frontend/src/components/FileInput.jsx
@@ -4,11 +4,11 @@ import AIFog from "./design/AIFog";
 
 function FileInput({id, invoice, onChange}) {
     return (
-        <div className="flex items-center justify-center w-full">
+        <div className="flex items-center justify-center min-w-[300px] lg:w-[400px]">
             {!invoice ? (
-                <div className="flex items-center justify-center w-full h-128 relative">
+                <div className="flex items-center justify-center w-full h-96 lg:h-128 relative">
                     <AIFog />
-                    <label htmlFor={id} className="flex flex-col items-center justify-center w-full h-128 rounded-2xl cursor-pointer bg-white shadow-md hover:bg-gray-100 transition-all relative opacity-80">
+                    <label htmlFor={id} className="flex flex-col items-center justify-center w-full h-full rounded-2xl cursor-pointer bg-white shadow-md hover:bg-gray-100 transition-all relative opacity-80">
                         <div className="flex flex-col items-center justify-center pt-5 pb-6">
                             <FontAwesomeIcon icon={faUpload} className="text-gray-500 text-2xl mb-2" />
                             <p className="mb-2 text-sm text-gray-500"><span className="font-semibold">Click to upload</span></p>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,12 +9,12 @@ function Navbar() {
                     <span class="self-center text-2xl font-semibold whitespace-nowrap">CompareDocs</span>
                 </div>
                 <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                    <ul class="flex flex-col p-4 md:p-0 mt-4 font-light border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+                    <ul class="flex flex-col gap-2 p-4 md:p-0 mt-4 font-normal border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
                     <li>
-                        <a href="#" class="block p-0 text-gray-900 rounded-sm hover:bg-gray-100">Home</a>
+                        <a href="#" class="block text-gray-800 rounded-sm transition-transform duration-150 hover:scale-105 hover:text-gray-900">Home</a>
                     </li>
                     <li>
-                        <a href="#" class="block p-0 text-gray-900 rounded-sm hover:bg-gray-100">About</a>
+                        <a href="#" class="block text-gray-800 rounded-sm transition-transform duration-150 hover:scale-105 hover:text-gray-900">About</a>
                     </li>
                     </ul>
                 </div>
