ed6b71d593abc9c0cac1eafbb3317314a22ae99e
BrindzaB <63659791+BrindzaB@users.noreply.github.com>
2025-10-07 18:54:36 +0200

    loadingPage basics implemented

---
+++ frontend/src/components/LoadingPage.jsx
@@ -0,0 +1,11 @@
+import LoaderElement from "./LoaderElement";
+
+function LoadingPage() {
+    return (
+        <div className="h-screen w-screen flex justify-center items-center">
+            <LoaderElement />
+        </div>
+    )
+}
+
+export default LoadingPage;
